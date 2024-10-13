b00471d03661300a628a4e099c50bd3b0f8b3eff
Brian Donnoe <dunow33@hotmail.com>
2024-10-13 15:00:49 -0400

    I think its working

---
+++ FriendContact/Migrations/20241013182114_new database connection.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FriendContact.Migrations
+{
+    /// <inheritdoc />
+    public partial class newdatabaseconnection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Friends",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastContactDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    DesiredContactFrequency = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Friends", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Friends_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Friend" },
+                    { 2, "Best-Friend" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Friends",
+                columns: new[] { "Id", "CategoryId", "DesiredContactFrequency", "LastContactDate", "Name" },
+                values: new object[] { 1, 1, 3, new DateOnly(2024, 10, 13), "John" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Friends_CategoryId",
+                table: "Friends",
+                column: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Friends");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
